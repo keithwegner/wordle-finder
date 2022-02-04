db733cba623c9cfae297dddb8305a388f4756019
Keith Wegner <42281599+keithwegner@users.noreply.github.com>
2022-02-03 22:31:12 -0500

    Update syntax

--- finder.py
+++ finder.py
@@ -1,23 +1,18 @@
 #!/usr/bin/env python
-
 import pandas
+import re
 
 with open("words.txt") as f:
-    
     data = []
-    words = {}
-    
+    words = {}    
     for line in f:
         line = line.strip("\n").lower()
         if len(line) == 5:
             if re.match("[a-z]{5}", line):
-                words[line] = 0
-                
+                words[line] = 0                
                 for char in line:
                     data.append(char)
-                    
     df = pandas.DataFrame(data)
-    
     count_value = df.value_counts()
     for word in words:
         for char in word:
@@ -25,7 +20,5 @@ with open("words.txt") as f:
                 words[word] -= count_value[char]
             else:
                 words[word] += count_value[char]
-    
     df2 = pandas.DataFrame(words.items())
-    
     print(df2.sort_values(by=1, ascending=False).head(20))
