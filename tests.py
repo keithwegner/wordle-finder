95c808825e5b5678c31dc6023692c254518d669b
Keith Wegner <keith.wegner@gmail.com>
2022-02-06 15:47:57 -0500

    Add super thorough tests

--- tests.py
+++ tests.py
@@ -1,6 +1,12 @@
-def test_true():
-    assert True==True
 
-if __name__ == "__main__":
-    test_true()
-    print("Everything passed")
+import unittest
+
+''' Just test something for CodeCov tool '''
+class Testing(unittest.TestCase):
+    def test_boolean(self):
+        a = True
+        b = True
+        self.assertEqual(a, b)
+
+if __name__ == '__main__':
+    unittest.main()
