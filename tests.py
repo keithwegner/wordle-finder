a893cde36e256c0521d1f5d9dcd7481bb189ccba
keithwegner <keith.wegner@gmail.co`m>
2022-02-07 03:18:07 +0000

    fixup: Format Python code with Black

--- tests.py
+++ tests.py
@@ -1,11 +1,14 @@
-''' Just test something for CodeCov tool '''
+""" Just test something for CodeCov tool """
 import unittest
 
+
 class Testing(unittest.TestCase):
-    ''' Class docstring'''
+    """Class docstring"""
+
     def test_boolean(self):
-        ''' Tests docstring '''
+        """Tests docstring"""
         self.assertEqual(True, True)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
